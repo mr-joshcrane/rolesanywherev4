e478d1edd86b6c03c22ca1e6a4ef4dfd7d0de5b0
Josh Crane <josh.crane@mechanicalrock.io>
2022-08-04 21:19:48 +0800

    Cleanup

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"crypto/sha256"
-	"crypto/x509"
 	"encoding/base64"
-	"encoding/hex"
 	"encoding/pem"
 	"fmt"
 	"io"
@@ -14,78 +11,13 @@ import (
 	"strings"
 	"time"
 
-	"github.com/google/go-cmp/cmp"
 	"github.com/mr-joshcrane/sigv4"
-	"golang.org/x/crypto/ssh"
 )
 
 func main() {
 	t := time.Now()
-	got := Mine(t)
-	want := Aidens(t)
+	Mine(t)
 
-	fmt.Println(cmp.Diff(want, got))
-}
-func Aidens(t time.Time) string {
-
-	q := url.Values{}
-	q.Set("profileArn", "arn:aws:rolesanywhere:ap-southeast-2:038021827431:profile/9419ce03-14c5-41e5-b0bc-62e717c53092")
-	q.Set("roleArn", "arn:aws:iam::038021827431:role/TestRole")
-	q.Set("trustAnchorArn", "arn:aws:rolesanywhere:ap-southeast-2:038021827431:trust-anchor/8f916267-7377-4d5d-a6f6-0b03f3feed3c")
-	u := fmt.Sprintf("https://rolesanywhere.ap-southeast-2.amazonaws.com/sessions?%s", q.Encode())
-	// inputPayload, _ := json.Marshal(map[string]any{"durationSeconds": 900})
-	req, err := http.NewRequest(http.MethodPost, u, http.NoBody)
-	if err != nil {
-		panic(err)
-	}
-
-	certPEM, err := ioutil.ReadFile("./certs/inter.pem")
-	if err != nil {
-		panic(err)
-	}
-	cert, _ := pem.Decode(certPEM)
-	c, err := x509.ParseCertificate(cert.Bytes)
-	if err != nil {
-		panic(err)
-	}
-
-	privateKey, err := ioutil.ReadFile("./certs/inter_key.pem")
-	if err != nil {
-		panic(err)
-	}
-	p, _ := pem.Decode(privateKey)
-
-	parsedKey, err := x509.ParsePKCS1PrivateKey(p.Bytes)
-
-	signer, err := ssh.NewSignerFromKey(parsedKey)
-	if err != nil {
-		panic(err)
-	}
-	sshsign := sigv4.SSHSigner{AlgorithmSigner: signer.(ssh.AlgorithmSigner)}
-
-	payloadHashHex := hex.EncodeToString(sigv4.MakeHash(sha256.New(), []byte{}))
-
-	err = sigv4.SignHTTP(
-		c,
-		&sshsign,
-		req,
-		payloadHashHex,
-		"rolesanywhere",
-		"ap-southeast-2",
-		t,
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	client := &http.Client{}
-
-	// fmt.Println(req)
-	resp, err := client.Do(req)
-	fmt.Println(resp, err)
-	bdy, _ := io.ReadAll(resp.Body)
-	fmt.Println(string(bdy))
-	return req.Header.Get("Authorization")
 }
 
 func Mine(t time.Time) string {
@@ -117,24 +49,12 @@ func Mine(t time.Time) string {
 	}
 	signedHeaders := strings.Join(sigv4.SignedHeaders(*req), ";")
 	cr := sigv4.CreateCanonicalRequest(*req)
-	fmt.Println(cr)
-
-	// p, _ := pem.Decode(keyPEM)
-
-	// parsedKey, err := x509.ParsePKCS1PrivateKey(p.Bytes)
-	// signer, err := ssh.NewSignerFromKey(parsedKey)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// sshsign := sigv4.SSHSigner{AlgorithmSigner: signer.(ssh.AlgorithmSigner)}
 
 	certSerial := "2"
 
 	credScope := t.Format("20060102") + "/ap-southeast-2/rolesanywhere/aws4_request"
 	hash := sigv4.HashedCanonicalRequest(cr)
 	stringToSign := sigv4.CreateStringToSign(*req, credScope, hash)
-
-	// fmt.Println(stringToSign)
 
 	credential := fmt.Sprintf("%s/%s", certSerial, credScope)
 	signature := sigv4.GetSignature(*req, stringToSign, keyPEM)
@@ -143,7 +63,6 @@ func Mine(t time.Time) string {
 
 	client := &http.Client{}
 	req.Header.Add("content-type", "application/json")
-	// fmt.Println(req)
 	resp, err := client.Do(req)
 	fmt.Println(resp, err)
 	bdy, _ := io.ReadAll(resp.Body)
