91db27df828b322d96bb21029756afe99394dac6
Josh Crane <josh.crane@mechanicalrock.io>
2022-08-05 21:08:26 +0800

    further cleaning

--- cmd/main.go
+++ cmd/main.go
@@ -10,14 +10,20 @@ import (
 )
 
 func main() {
-	profileArn := "arn:aws:rolesanywhere:ap-southeast-2:038021827431:profile/9419ce03-14c5-41e5-b0bc-62e717c53092"
-	roleArn := "arn:aws:iam::038021827431:role/TestRole"
-	trustAnchorArn := "arn:aws:rolesanywhere:ap-southeast-2:038021827431:trust-anchor/8f916267-7377-4d5d-a6f6-0b03f3feed3c"
+	profileArn := "arn:aws:rolesanywhere:ap-southeast-2:123456789012:profile/9419ce03-14c5-41e5-b0bc-62e717c53092"
+	roleArn := "arn:aws:iam::123456789012:role/TestRole"
+	trustAnchorArn := "arn:aws:rolesanywhere:ap-southeast-2:123456789012:trust-anchor/8f916267-7377-4d5d-a6f6-0b03f3feed3c"
 	region := "ap-southeast-2"
 	signingCertPath := "./certs/inter.pem"
 	signingKeyPath := "./certs/inter_key.pem"
 
-	req, err := sigv4.SignedRequest(region, profileArn, roleArn, trustAnchorArn, signingCertPath, signingKeyPath)
+	signingCert, signingKey, err := sigv4.LoadCertsKeys(signingCertPath, signingKeyPath)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	req, err := sigv4.SignRequest(region, profileArn, roleArn, trustAnchorArn, signingCert, signingKey)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -32,10 +38,3 @@ func main() {
 	fmt.Println(string(bdy))
 
 }
-
-// Take profileArn, roleArn, trustAnchorArn as url.Values
-// Create a post request
-// read in cert
-// read in key
-// Attach the headers
-// authorize the request
