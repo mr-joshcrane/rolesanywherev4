cff5e1d3fa6b3a7032c03715dbafa0faccd07cb4
Josh Crane <josh.crane@mechanicalrock.io>
2022-08-05 21:07:37 +0800

    Refactoring and adding test

--- sigv4_test.go
+++ sigv4_test.go
@@ -17,20 +17,21 @@ func TestSignRequest(t *testing.T) {
 	roleArn := "arn:aws:iam::123456789012:role/TestRole"
 	trustAnchorArn := "arn:aws:rolesanywhere:ap-southeast-2:123456789012:trust-anchor/8f916267-7377-4d5d-a6f6-0b03f3feed3c"
 	region := "ap-southeast-2"
-	signingCertPath := "./certs/inter.pem"
-	signingKeyPath := "./certs/inter_key.pem"
-
-	signingCert, signingKey, err := sigv4.LoadCertsKeys(signingCertPath, signingKeyPath)
+	signingCert, err := Cert()
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	signingKey, err := Key()
+	if err != nil {
+		t.Fatal(err)
+	}
 	req, err := sigv4.SignRequest(region, profileArn, roleArn, trustAnchorArn, signingCert, signingKey)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	want := "AWS4-X509-RSA-SHA256 Credential=2/20000101/ap-southeast-2/rolesanywhere/aws4_request, SignedHeaders=host;x-amz-date;x-amz-x509, Signature=1ae8d28706c17ae0878bb0dcab0ec7975f1c49a82a80c7a4eb6fa7cfc4b8bce64607fd9965dd119a27fbc39065fa30600872575005b6ccf39c2d4fa7a92d9d7c7953d41bb06f3b44e19b0b410f0c62f2b7f838c9158281cbae4489ea4152ee101ffb0827bad7d9bc7bf52b1351f9a0f2628b4a10c0991e6e2b84a22e9729e73d4b270cec046682036c03a33ce4f8a6b76a82f0146c70c64568df5899aab455af27dffc8b9cbb920fd3d97e323d0d3fcbd71eff7a6677821ae5842c5ffab5bca77171c8af2127733c03e1cad18199f6e3d4cd53cbafcc6913da35708d6c40e3d107274d094a892d9c56fe70424ba057241e47f4e23ff55577efa1da2b43284f38"
+	want := "AWS4-X509-RSA-SHA256 Credential=2/20000101/ap-southeast-2/rolesanywhere/aws4_request, SignedHeaders=host;x-amz-date;x-amz-x509, Signature=60e605c8436fe98f19bed796d6e6555337b12ebf8f3e2ea17f83b58a1de156002e5bd218692906639abe0f69c4765b137e35641bfa66c7f6cf2f48251369dbab12e83cdf94d4f2eedb8496c2fc219bc7cb4bb2725451e264f9f90da18591f8e089edaa5d585cb790169491e5131c3127bc55897a4fc99121fdfee0ae854db3038573c3e01007a993932df8f5a0cdf94c2ee651893f30e8c4ed5a94afd81b4e05a24d16f36a71185196a22e381bd47daaeafd38b66bb388c9ceda6695f8fe34b4d97e1603b1caf549dd1260f0be9b0a8e8da5cfeb5d5356222550bb58b7dd752651e0aa23012a1256d83020ad07397dde0c5d47f94d618774c02d1404746b8cae"
 	got := req.Header.Get("Authorization")
 	if want != got {
 		t.Fatalf(cmp.Diff(want, got))
